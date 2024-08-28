7958efb3d91deaf33bb6965aa49e711b20e7ab68
PrinceShlime <aryashreepradhan@gmail.com>
2024-08-28 14:56:33 -0400

    Finished CQ00

---
+++ CQs/cq00_functions.py
@@ -0,0 +1,15 @@
+"""First Challange Question of Comp 110"""
+
+__author__ = "730740774"
+
+
+def mimic(message: str) -> str:
+    return message
+
+
+def main() -> None:
+    print(mimic(message=input("What is your message?")))
+
+
+if __name__ == "__main__":
+    main()
