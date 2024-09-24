89fadb020711602f3f7de107f99db36c417f6a5d
PrinceShlime <aryashreepradhan@gmail.com>
2024-09-24 17:12:46 -0400

    Actually finished ex02 Chardle

--- exercises/ex02_chardle.py
+++ exercises/ex02_chardle.py
@@ -4,16 +4,16 @@ __author__ = "730740774"
 
 
 def main() -> None:
-    contains_char(hidden_word=input_word(), user_guess=input_letter())
+    contains_char(user_word=input_word(), user_guess=input_letter())
 
 
 def input_word() -> str:
-    user_word: str = str(input("Enter a 5-character word: "))
+    word: str = str(input("Enter a 5-character word: "))
     # Instead of creating a nested 'if' statement I use the 'or' operator
-    if len(user_word) < 5 or len(user_word) > 5:
+    if len(word) < 5 or len(word) > 5:
         print("Error: Word must contain 5 characters.")
         exit()
-    return user_word
+    return word
 
 
 def input_letter() -> str:
@@ -25,33 +25,37 @@ def input_letter() -> str:
     return user_letter
 
 
-def contains_char(hidden_word: str, user_guess: str) -> None:
+def contains_char(user_word: str, user_guess: str) -> None:
     count: int = 0
-    print(f"Searching for {user_guess} in {hidden_word}")
-    if hidden_word[0] == user_guess:
+    print(f"Searching for {user_guess} in {user_word}")
+    if user_word[0] == user_guess:
         print(f"{user_guess} found at index 0")
         count += 1
 
-    if hidden_word[1] == user_guess:
+    if user_word[1] == user_guess:
         print(f"{user_guess} found at index 1")
         count += 1
 
-    if hidden_word[2] == user_guess:
+    if user_word[2] == user_guess:
         print(f"{user_guess} found at index 2")
         count += 1
 
-    if hidden_word[3] == user_guess:
+    if user_word[3] == user_guess:
         print(f"{user_guess} found at index 3")
         count += 1
 
-    if hidden_word[4] == user_guess:
+    if user_word[4] == user_guess:
         print(f"{user_guess} found at index 4")
         count += 1
 
     if count == 0:
-        print(f"No instances of {user_guess} found in {hidden_word}")
+        print(f"No instances of {user_guess} found in {user_word}")
+    # Forgot to take into account if 'count' equaled one
+    # and how instances would change to instance
+    elif count == 1:
+        print(f"1 instance of {user_guess} found in {user_word}")
     else:
-        print(f"{count} instances of {user_guess} found in {hidden_word}")
+        print(f"{count} instances of {user_guess} found in {user_word}")
 
 
 if __name__ == "__main__":
